7b21c7ff6b2cff7b084081d25ef162e55fd5591f
Tarssito <tarssito@gmail.com>
2018-10-06 19:11:38 -0300

    Entity User e Rest funcionando

---
+++ src/main/java/com/tarssito/restmongo/RestMongoApplication.java
@@ -0,0 +1,12 @@
+package com.tarssito.restmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMongoApplication.class, args);
+	}
+}
