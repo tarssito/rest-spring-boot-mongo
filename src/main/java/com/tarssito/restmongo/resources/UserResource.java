07d35129a2316cc9ac26024145a858c27ff52ffc
Tarssito <tarssito@gmail.com>
2018-10-07 18:38:42 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/tarssito/restmongo/resources/UserResource.java
+++ src/main/java/com/tarssito/restmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.tarssito.restmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tarssito.restmongo.domain.User;
+import com.tarssito.restmongo.dto.UserDTO;
 import com.tarssito.restmongo.services.UserService;
 
 @RestController
@@ -17,10 +19,12 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listdto = list.stream()
+				.map(u -> new UserDTO(u)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listdto);
 	}
 }
