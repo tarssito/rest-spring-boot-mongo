904a277a992971d63f150236f3acbadcb8486291
Tarssito <tarssito@gmail.com>
2018-10-23 22:33:05 -0300

    Obtendo um usuário por id

--- src/main/java/com/tarssito/restmongo/services/UserService.java
+++ src/main/java/com/tarssito/restmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.tarssito.restmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.tarssito.restmongo.domain.User;
 import com.tarssito.restmongo.repositories.UserRepository;
+import com.tarssito.restmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
