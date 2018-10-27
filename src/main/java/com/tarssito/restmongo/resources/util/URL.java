3689d402a016e92994ddf0004ffbf4e8b8945869
Tarssito <tarssito@gmail.com>
2018-10-27 15:36:08 -0300

    Consulta com vários critérios

--- src/main/java/com/tarssito/restmongo/resources/util/URL.java
+++ src/main/java/com/tarssito/restmongo/resources/util/URL.java
@@ -2,9 +2,14 @@ package com.tarssito.restmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Classe utilizada para decodificar parametros de url concatenada no JS
+ * 
  * @author tarssito
  *
  */
@@ -15,6 +20,16 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
 		}
 	}
 }
