49d3fa9387978c35cf9739efc99fece55985ed4d
Tarssito <tarssito@gmail.com>
2018-10-27 11:03:10 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/tarssito/restmongo/config/Instantiation.java
+++ src/main/java/com/tarssito/restmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.tarssito.restmongo.domain.Post;
 import com.tarssito.restmongo.domain.User;
+import com.tarssito.restmongo.dto.AuthorDTO;
 import com.tarssito.restmongo.repositories.PostRepository;
 import com.tarssito.restmongo.repositories.UserRepository;
 
@@ -34,14 +35,15 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", 
-				"vou viajar pra São Paulo. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", 
-				"Acordei feliz hoje!", maria);
-
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", 
+				"vou viajar pra São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", 
+				"Acordei feliz hoje!", new AuthorDTO(maria));
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
 	}
 
 }
