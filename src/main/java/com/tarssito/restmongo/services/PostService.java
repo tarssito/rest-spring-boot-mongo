91eaaff87b1246b1f69428259465f414d400e1c1
Tarssito <tarssito@gmail.com>
2018-10-27 11:51:51 -0300

    Obtendo um post por id

---
+++ src/main/java/com/tarssito/restmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.tarssito.restmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tarssito.restmongo.domain.Post;
+import com.tarssito.restmongo.repositories.PostRepository;
+import com.tarssito.restmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> entity = repository.findById(id);
+		return entity.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+}
